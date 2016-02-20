2ed2caac1973dbde67bf65ccbb4e08f58c9c9f1a
Joe Yeung <jyeung001@gmail.com>
2016-02-20 17:32:34 -0500

    UPDATE pagination goodness for Events#index
    Use will_paginate gem to enable pagination. Add helper method
    ApplicationController#set_pagination to be called before
    every action to set pagination parameters.
    
    Add a json view for Events#index, also specify default response
    is json.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_pagination
+
+  private
+
+  def set_pagination
+    @pagination = {}
+
+    page_param = params[:offset] || params[:page]
+    @pagination[:page] = page_param ? page_param.to_i : nil
+    @pagination[:per_page] = params[:limit].to_i if params[:limit]
+  end
 end
